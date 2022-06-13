2b80826d25694629f224f87cfae8ce5e529974dc
Mezisashe Ojuba <sasheojuba@fb.com>
2022-06-13 15:07:37 -0700

    implemented functionality for user to log in and out of their accout

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -2,10 +2,16 @@ package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,17 +19,50 @@ public class LoginActivity extends AppCompatActivity {
     Button btnLogin;
     EditText etTypeUsername;
     EditText etTypePassword;
+    String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // check if user has already logged in
+        if (ParseUser.getCurrentUser() != null){
+            // if user is logged in, go directly to main page
+            goToMainActivity();
+        }
+
+
+        btnLogin = findViewById(R.id.btnLogin);
+        etTypePassword = findViewById(R.id.etTypePassword);
+        etTypeUsername = findViewById(R.id.etTypeUsername);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // what will happen when user clicks the login button
+                String username = etTypeUsername.getText().toString();
+                String password = etTypePassword.getText().toString();
+                ParseUser.logInInBackground(username, password, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (e != null){
+                            Log.e(TAG, "issue with login");
+                        }
+                        else
+                        {
+                            Log.d(TAG, "login successful");
+                            goToMainActivity();
+                        }
+                    }
+                });
+
             }
         });
+    }
+
+    private void goToMainActivity(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/parstagram/MainActivity.java
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            // if user clicks on logout button located in the menu
+            case R.id.miLogout:
+                //todo: logout user
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+                // go back to the login page after user is logged out
+                goToLoginPage();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void goToLoginPage(){
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
