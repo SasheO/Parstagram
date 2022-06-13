5fc49c03754724f1cd7f9f30846610cdbd0addc4
Mezisashe Ojuba <sasheojuba@fb.com>
2022-06-13 16:54:46 -0700

    created method to query parse for posts

--- app/src/main/java/com/example/parstagram/NewPostActivity.java
+++ app/src/main/java/com/example/parstagram/NewPostActivity.java
@@ -4,18 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class NewPostActivity extends AppCompatActivity {
 
+    private static final String TAG = "NewPostActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
 
+        queryPosts();
+
+    }
+
+    private void queryPosts() {
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER); // not sure what this does, look it up
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "error retrieving posts", e);
+                }
+                else{
+                    // then getting posts from the database was successful\
+                    // log the description of each post
+                    for (Post post : posts){
+                        Log.i(TAG, post.getDescription() + ", username: " + post.getUser().getUsername());
+                    }
+                }
+            }
+        });
     }
 
     @Override
