510c367c1b4a221c7678f02e91b3b4042e8986fd
Mezisashe Ojuba <sasheojuba@fb.com>
2022-06-13 16:10:54 -0700

    created getter and setter methods in Post class that extends ParseObject

--- app/src/main/java/com/example/parstagram/Post.java
+++ app/src/main/java/com/example/parstagram/Post.java
@@ -1,6 +1,7 @@
 package com.example.parstagram;
 
 import com.parse.ParseClassName;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
@@ -10,8 +11,32 @@ import java.io.File;
 @ParseClassName("Post")
 public class Post extends ParseObject {
 
-    // todo: insert image attribute
-    private String description;
-    private ParseUser user;
+    // these variables store the name that will be used to send queries to Parse database
+    public static final String KEY_DESCRPTION = "description";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_USER = "user";
 
+    public String getDescription(){
+        return getString(KEY_DESCRPTION);
+    }
+
+    public ParseUser getUser(){
+        return getParseUser(KEY_USER);
+    }
+
+    public ParseFile getImage(){
+        return getParseFile(KEY_IMAGE);
+    }
+
+    public void setDescription(String description){
+        put(KEY_DESCRPTION, description);
+    }
+
+    public void setUser(ParseUser user){
+        put(KEY_USER, user);
+    }
+
+    public void setImage(ParseFile image){
+        put(KEY_IMAGE, image);
+    }
 }
