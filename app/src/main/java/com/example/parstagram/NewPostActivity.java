024dde2deda7c799fcdbab743dca48e61823d7f3
Mezisashe Ojuba <sasheojuba@fb.com>
2022-06-14 15:08:10 -0700

    added functionality to take pictures

--- app/src/main/java/com/example/parstagram/NewPostActivity.java
+++ app/src/main/java/com/example/parstagram/NewPostActivity.java
@@ -1,15 +1,22 @@
 package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
@@ -18,11 +25,16 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.io.File;
 import java.util.List;
 
 public class NewPostActivity extends AppCompatActivity {
 
     private static final String TAG = "NewPostActivity";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+    private File photoFile;
+    public String photoFileName = "photo.jpg";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +45,16 @@ public class NewPostActivity extends AppCompatActivity {
         Button btnSubmitPost = findViewById(R.id.btnSubmitPost);
         EditText etTypeDescription = findViewById(R.id.etTypeDescription);
         Button btnTakePicture = findViewById(R.id.btnTakePicture);
+
+        // when user tries to take picture for post
+        btnTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchCamera();
+            }
+        });
+
+        // when user tries to submit post
         btnSubmitPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,14 +71,13 @@ public class NewPostActivity extends AppCompatActivity {
                     newPost.saveInBackground(new SaveCallback() {
                         @Override
                         public void done(ParseException e) {
-                            if (e != null){
+                            if (e != null){ // if there was an exception returned while trying to save the post to server
                                 Log.e(TAG, "error encountered saving image to server: " + e.toString());
                                 Toast.makeText(NewPostActivity.this, "sorry! could not upload post. try again...", Toast.LENGTH_SHORT).show();
 
                             }
-                            else{
-                                Log.i(TAG, "successfully saved post");
-                                Toast.makeText(NewPostActivity.this, "successfully posted!", Toast.LENGTH_SHORT).show();
+                            else{ // if no error returned while trying to save post to server
+                                etTypeDescription.setText(null);
 
                             }
                         }
@@ -70,6 +91,64 @@ public class NewPostActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    private void launchCamera() {
+        // todo: launch the camera with an intent, designating a file path, and handle the onActivityResult.
+        // guide here: https://guides.codepath.org/android/Accessing-the-Camera-and-Stored-Media
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(NewPostActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ImageView ivPreview = (ImageView) findViewById(R.id.ivPicturePreview);
+                ivPreview.setImageBitmap(takenImage);
+                ivPreview.setVisibility(View.VISIBLE);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void queryPosts() {
